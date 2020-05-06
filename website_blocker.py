24c3126a53e4ffa4e5da6a5d178985b25adc353c
George Willens <willensgeorge@gmail.com>
2020-05-06 18:22:41 +0100

    Initial commit

---
+++ website_blocker.py
@@ -0,0 +1,12 @@
+import time
+from datetime import datetime as dt
+
+hosts_path = "/private/etc/hosts"
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com", "facebook.com",
+                "www.instagram.com", "instagram.com"]
+
+while True:
+    print(1)
+    print(dt.now())
+    time.sleep(5)
